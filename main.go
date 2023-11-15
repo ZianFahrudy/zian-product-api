9f4355280076784dd50cab72a3ce43e05785fb66
ZianF <zianfahrudy@gmail.com>
2023-11-15 22:53:01 +0800

    update main

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 	authController := controller.NewAuthController(&authService, authRepository, configuration)
 	productController := controller.NewProductController(&productService, productRepository, authRepository, configuration)
 
-	gin.SetMode(gin.DebugMode)
+	gin.SetMode(gin.ReleaseMode)
 	app := gin.Default()
 	app.Static("/storage", "./storage")
 	app.Use(gin.CustomRecovery(exception.ErrorHandler))
